63893987e6064bad026e62a0c80467454061a6e5
Sean Gao <y_gao@umail.ucsb.edu>
2020-03-28 23:29:29 -0700

    updated colors dictionary to match with state variable

--- constants.py
+++ constants.py
@@ -1,26 +1,33 @@
-colors = {
-    'r': (255, 0, 0),  # red
-    'g': (0, 255, 0),  # green
-    'b': (0, 0, 255),  # blue
-    'y': (255, 255, 0),  # yellow
-    'c': (0, 255, 255),  # cyan
-    'm': (255, 0, 255),  # magenta
-    'w': (255, 255, 255),  # white
-    'k': (0, 0, 0)  # black
+COLORS = {
+    0: (0, 0, 255),  # blue                   HEALTHY
+    1: (255, 0, 0),  # red                    INFECTED
+    2: (255, 255, 0),  # yellow               ASYMPOMATIC
+    3: (255, 255, 255),  # white              IMMUNE
+    4: (0, 255, 0),  # green
+    5: (0, 255, 255),  # cyan
+    6: (255, 0, 255),  # magenta
+    7: (0, 0, 0),  # black
+    8: (128, 128, 128) # gray
 }
 
 
 
-RADIUS = 10
+RADIUS = 3
 MAX_VEL = 3
 MAX_ACCEL = 0.5
 
 START_WIDTH = 800
 START_HEIGHT = 600
 START_PERSONS = 100
+
 START_PERCEPTION = 35
 START_STEERING = 0.2
+
+START_RAD_I = 20
+START_RATE_I = 0.2
+
 RANDOM_ACCEL_COE = 3 # higher is less random
 ACCEL_DECAY = 0.8    # lower is more decay
 
-SHOW_VELOCITY = 1
+SHOW_VELOCITY = 0
+
